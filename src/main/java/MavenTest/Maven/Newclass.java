f1b8f7aa3460a54ee35f877f5cd8fa1f60802fb5
himadri <dighehimadri@gmail.com>
2017-09-10 17:36:59 -0400

    adding in previous project

---
+++ src/main/java/MavenTest/Maven/Newclass.java
@@ -0,0 +1,25 @@
+package MavenTest.Maven;
+
+public class Newclass {
+	
+	public void method3() 
+	{
+		System.out.println("3 method");
+		
+	}
+	public void method4()
+	{
+	System.out.println("method 4");
+		
+	}
+
+	public static void main(String[] args) 
+	{
+	Newclass p = new Newclass();
+		p.method3();
+		p.method4();
+		System.out.println("print all method AND FOLDER");
+
+	}
+
+}
